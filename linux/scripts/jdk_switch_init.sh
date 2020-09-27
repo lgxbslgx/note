c83d9ea71986bc257ebbf9145359f46d12328c86
Guoxiong Li <lgxbslgx@gmail.com>
2020-09-27 20:25:14 +0800

    Add directory scripts. Add script jdk_switch.

---
+++ linux/scripts/jdk_switch_init.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+sudo update-alternatives --install /usr/bin/java java /home/lgx/source/java/jdk8u-dev/build/linux-x86_64-normal-server-release/images/j2sdk-image/bin/java 1080
+
+sudo update-alternatives --install /usr/bin/java java /home/lgx/source/java/jdk11u-dev/build/linux-x86_64-normal-server-release/images/jdk/bin/java 1080
+
+sudo update-alternatives --install /usr/bin/java java /home/lgx/source/java/jdk12u/build/linux-x86_64-server-release/images/jdk/bin/java 1080
+
+sudo update-alternatives --install /usr/bin/java java /home/lgx/source/java/jdk13u/build/linux-x86_64-server-release/images/jdk/bin/java 1080
+
+sudo update-alternatives --install /usr/bin/java java /home/lgx/source/java/jdk14u/build/linux-x86_64-server-release/images/jdk/bin/java 1080
+
+sudo update-alternatives --install /usr/bin/java java /home/lgx/source/java/jdk15u/build/linux-x86_64-server-release/images/jdk/bin/java 1080
+
+sudo update-alternatives --install /usr/bin/java java /home/lgx/source/java/jdk/build/linux-x86_64-server-release/images/jdk/bin/java 1080
+
